410c4ac75c76fcc51eaef016ac838f85d109e356
Erica Boyer <erica.boyer29@gmail.com>
2016-10-08 21:37:14 -0400

    idk

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(".dropdown-menu li a").click(function(){
 
-  $(this).parents(".btn-group").find('.selection').text($(this).text());
-  $(this).parents(".btn-group").find('.selection').val($(this).text());
+  $(this).parents(".dropdown").find('.dropdown-text').text($(this).text());
+  $(this).parents(".dropdown").find('.dropdown-menu').val($(this).text());
 
 });
